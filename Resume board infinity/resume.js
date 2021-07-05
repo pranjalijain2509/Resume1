33c3a912d026a43a7fbdfdcb34eee736e2356753
pranjalijain2509 <65130968+pranjalijain2509@users.noreply.github.com>
2021-07-05 17:53:05 +0530

    Add files via upload

---
+++ Resume board infinity/resume.js
@@ -0,0 +1,216 @@
+function myFunction() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "images/pp.jpg");
+    x.setAttribute("width", "1265");
+    x.setAttribute("height", "408");
+    x.setAttribute("alt", " Rock");
+    document.body.appendChild(x);
+  }
+
+  function myFunction1() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "images/my_photo.jpg");
+    x.setAttribute("class","demo");
+   
+    document.body.appendChild(x);
+  }
+
+  function myFunction2() {
+    var x = document.createElement("H3");
+    var t = document.createTextNode("Pranjali Jain");
+    x.appendChild(t);
+    x.setAttribute("class","nam");
+   
+    document.body.appendChild(x);
+  }
+
+
+  function myFunction3() {
+    var x = document.createElement("P");
+    var t = document.createTextNode('I am Pranjali Jain second-year student and I am currently pursuing computer science engineering from Lovely Professional University, Punjab. I have been a part of various organizations and had organized events and recently gave a talk to young minds regarding Hackathon. I am a fast learner and eager to learn new things daily to convert my dreams into reality. I an enthusiastic Techgeek with great interest in Web Development and keep on striving to innovate new things that could help society.');
+    x.appendChild(t);
+    x.setAttribute("class","para");
+   
+    document.body.appendChild(x);
+  }
+
+
+  function myFunction4() {
+    var x = document.createElement("H1");
+  
+    var t = document.createTextNode("Skills");
+    x.appendChild(t);
+    x.setAttribute("class","fas fa-chalkboard-teacher");
+
+    document.body.appendChild(x);
+  }
+  
+
+  function myFunction5() 
+  {
+    var x = document.createElement("h4");
+    var t = document.createTextNode("HTML:80%");
+    x.appendChild(t);
+    x.setAttribute("class","skil");
+
+    document.body.appendChild(x);
+  }
+
+  function myFunction20() 
+  {
+    var x = document.createElement("h4");
+    var t = document.createTextNode("CSS:83%");
+    x.appendChild(t);
+    x.setAttribute("class","skil");
+    document.body.appendChild(x);
+  }
+
+  function myFunction21() 
+  {
+    var x = document.createElement("h4");
+    var t = document.createTextNode("JavaScript:75%");
+    x.appendChild(t);
+    x.setAttribute("class","skil");
+    document.body.appendChild(x);
+  }
+
+  function myFunction22() 
+  {
+    var x = document.createElement("h4");
+    var t = document.createTextNode("python:92%");
+    x.appendChild(t);
+    x.setAttribute("class","skil");
+    document.body.appendChild(x);
+  }
+
+  function myFunction6() {
+    var x = document.createElement("H1");
+  
+    var t = document.createTextNode("Work Experience");
+    x.appendChild(t);
+    x.setAttribute("class","fas fa-briefcase");
+
+    document.body.appendChild(x);
+  }
+
+  function myFunction7() {
+    var x = document.createElement("H1");
+  
+    var t = document.createTextNode("Give My Certificate ");
+    x.appendChild(t);
+    x.setAttribute("class","sd");
+    document.body.appendChild(x);
+  }
+
+  function myFunction8() {
+    var x = document.createElement("P");
+    var t = document.createTextNode('	Contributed greatly on the front-end part of the website and played a major role in maintaining the privacy policy page of the company and Designed various front web pages to conduct online competition which increasing user engagement by 15%');
+    x.appendChild(t);
+    x.setAttribute("class","para1");
+   
+    document.body.appendChild(x);
+  }
+
+  function myFunction9() {
+    var x = document.createElement("H1");
+  
+    var t = document.createTextNode("The Open Code Foundation ");
+    x.appendChild(t);
+    x.setAttribute("class","sd");
+    document.body.appendChild(x);
+  }
+
+  function myFunction10() {
+    var x = document.createElement("P");
+    var t = document.createTextNode('	Created impactful mini courses on fundamentals of python and Worked on front-end web development and developed various aspect of their website which resulted in increase of 30% user engagement ');
+    x.appendChild(t);
+    x.setAttribute("class","para1");
+   
+    document.body.appendChild(x);
+  }
+
+  function myFunction11() {
+    var x = document.createElement("H1");
+  
+    var t = document.createTextNode("IITR-CSIR");
+    x.appendChild(t);
+    x.setAttribute("class","sd");
+    document.body.appendChild(x);
+  }
+
+  function myFunction12() {
+    var x = document.createElement("P");
+    var t = document.createTextNode('	Completed projects mainly focusing on Tech-stacks like computational chemistry, RGB analysis and PCB designing and Worked on incorporating technologies like AI and Machine Level into conventional research to increase feasibility and error detection.  ');
+    x.appendChild(t);
+    x.setAttribute("class","para1");
+   
+    document.body.appendChild(x);
+  }
+
+
+  function myFunction13() {
+    var x = document.createElement("H1");
+  
+    var t = document.createTextNode("Education");
+    x.appendChild(t);
+    x.setAttribute("class","fas fa-university");
+
+    document.body.appendChild(x);
+  }
+
+
+  function myFunction14() {
+    var x = document.createElement("H1");
+  
+    var t = document.createTextNode("Lovely Professional University");
+    x.appendChild(t);
+    x.setAttribute("class","sd");
+    document.body.appendChild(x);
+  }
+
+  function myFunction15() {
+    var x = document.createElement("H2");
+  
+    var t = document.createTextNode("2019-2023");
+    x.appendChild(t);
+    x.setAttribute("class","sd");
+    document.body.appendChild(x);
+  }
+
+
+  function myFunction16() {
+    var x = document.createElement("P");
+    var t = document.createTextNode('	I am currently a second year  am an active member of women Tech makers from quite some time.I am a fast leaner and eager to learn new things daily so that I can convert my dreams into reality.I an enthusiastic Techgeek with great interest in Web Development and keep on striving to innovate new things that could help the society.');
+    x.appendChild(t);
+    x.setAttribute("class","para1");
+   
+    document.body.appendChild(x);
+  }
+
+
+  
+  function myFunction17() {
+    var x = document.createElement("H1");
+  
+    var t = document.createTextNode("Seth M.R Jaipuria School");
+    x.appendChild(t);
+    x.setAttribute("class","sd");
+    document.body.appendChild(x);
+  }
+
+  function myFunction18() {
+    var x = document.createElement("H2");
+  
+    var t = document.createTextNode("2005-2019");
+    x.appendChild(t);
+    x.setAttribute("class","sd");
+    document.body.appendChild(x);
+  }
+function myFunction19() {
+    var x = document.createElement("P");
+    var t = document.createTextNode('I was a part of various Technical society in my school days and have also organized various Inter school Events.');
+    x.appendChild(t);
+    x.setAttribute("class","para1");
+   
+    document.body.appendChild(x);
+  }
